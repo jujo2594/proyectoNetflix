1c5e22a2f864225c5e7ecf2c2f6b142ff14df22b
jujo2594 <105889688+jujo2594@users.noreply.github.com>
2023-08-24 23:55:48 -0500

    Modificacon Tarjetas-1

--- main.js
+++ main.js
@@ -16,6 +16,23 @@ let construirEncabezado = async()=>{
 
 construirEncabezado();
 
+let construirTarjetas = async()=>{
+    let peticion = await fetch(`${path}.json`);
+    let res = peticion.json();
+    let seleccion = document.querySelectorAll('#myJsonSection2');
+    seleccion.insertAdjacentHTML('beforeend', /* HTML*/ `     
+    ${res.section2.bloque.map((value)=> /* HTML */ `
+        <div class="card mb-4 shadow-sm">
+            <div class="py-3 bg-danger opacity-75 text-light">
+                <h4 class="my-0 fw-normal">Basic</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">$ 6,99<small    class="text-body-secondary fw-light">/mo</small></h1>
+            </div>
+        </div>
+    `).join(" ")}`)
+}
+
 
 // (async()=>{
 //      let peticion = await fetch(`${path}.json`);
