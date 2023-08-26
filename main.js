b2be1f7c4af4654d18e610b1439b2c7dc32fdcfd
jujo2594 <105889688+jujo2594@users.noreply.github.com>
2023-08-25 21:50:55 -0500

    Finalizacion de Tarjetas y NavBar

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 const path = 'config';
 
+let construirNavBar = async()=>{
+    let peticion = await fetch(`${path}.json`);
+    let res = await peticion.json();
+    let seleccion = document.querySelector('#myJsonNavBar');
+    seleccion.insertAdjacentHTML('beforeend',/* HTML */`   
+    <a href="/" class="d-flex align-items-center link-body-emphasis text-decoration-none">
+        <span class="fs-4">
+            <img src=${res.navBar.logo} alt="logo">
+        </span>
+    </a>
+    <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        <a class="py-2 link-body-emphasis text-decoration-none --bs-gray-600" href="#">${res.navBar.textoIngreso}</a>
+    </nav>
+    `)
+}
+
+construirNavBar();
+
 let construirEncabezado = async()=>{
     let peticion = await fetch(`${path}.json`);
     let res = await peticion.json();
@@ -18,20 +36,39 @@ construirEncabezado();
 
 let construirTarjetas = async()=>{
     let peticion = await fetch(`${path}.json`);
-    let res = peticion.json();
-    let seleccion = document.querySelectorAll('#myJsonSection2');
-    seleccion.insertAdjacentHTML('beforeend', /* HTML*/ `     
-    ${res.section2.bloque.map((value)=> /* HTML */ `
+    let res = await peticion.json();
+    let cards = res.bloque2;
+    let lastCard = res.bloque2[res.bloque2.length-1];
+    cards.pop();
+    let seleccion = document.querySelector('#myJsonSection2');
+        seleccion.insertAdjacentHTML('beforeend', /* HTML*/ `     
+        ${cards.map((value)=> /* HTML */ `
+            <div class="col">
+                <div class="card mb-4 shadow-sm">
+                    <div class="py-3 bg-danger opacity-75 text-light">
+                        <h4 class="my-0 fw-normal">${value.textoPlan}</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">${value.textoPrecio}<small    class="text-body-secondary fw-light">/mo</small></h1>
+                    </div>
+                </div>
+            </div>
+    `).join(" ")}`);
+    seleccion.insertAdjacentHTML('beforeend', /* HTML*/ ` 
+    <div class="col">
         <div class="card mb-4 shadow-sm">
-            <div class="py-3 bg-danger opacity-75 text-light">
-                <h4 class="my-0 fw-normal">Basic</h4>
+            <div class="py-3 text-bg-primary bg-danger border">
+                <h4 class="my-0 fw-normal">${lastCard.textoPlan}</h4>
             </div>
             <div class="card-body">
-                <h1 class="card-title pricing-card-title">$ 6,99<small    class="text-body-secondary fw-light">/mo</small></h1>
+                <h1 class="card-title pricing-card-title">${lastCard.textoPrecio}<small class="text-body-secondary fw-light">/mo</small></h1>
             </div>
         </div>
-    `).join(" ")}`)
+    </div>
+    `);
 }
+
+construirTarjetas();
 
 
 // (async()=>{
